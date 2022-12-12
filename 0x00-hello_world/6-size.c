4bf7e69cf11d9ea40f94fc0ea5e8b32294aec5b2
Haimanotgg <Haimanotgbb@gmail.com>
2022-12-11 23:48:17 -0800

    task 5

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -Entry
+ * Retrun : Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", size of(char));
+	printf("Size of a int: %i byte(s)\n", size of(int));
+	printf("Size of a long int: %i byte(s)\n", size of(long int));
+	printf("Size of a long long int: %i byte(s)\n", size of(long long int));
+	printf("Size of a float: %i byte(s)\n", size of(float));
+	return (0);
+	}
